1f8a408dec2d637d46f08c60797275876ad43870
Igor1101 <Igor1101@github.com>
2019-10-29 09:18:15 +0200

    lexems naming fixes

--- lexems/lex_utils.c
+++ lexems/lex_utils.c
@@ -10,43 +10,43 @@ char* lex_to_str(lexem_t lt)
 {
 	switch(lt) {
 	case L_STRING:
-		return "string";
+		return "STRING";
 	case L_CHAR:
-		return "char";
+		return "CHAR";
 	case L_STRUCT_DELIMITER:
-		return "struct delimiter";
+		return "STRUCT DELIMITER";
 	case L_STRUCT_POINTER:
-		return "struct pointer";
+		return "STRUCT POINTER";
 	case L_POINTER:
-		return "pointer";
+		return "POINTER";
 	case L_CONSTANT_FLOAT:
-		return "constant float";
+		return "CONSTANT FLOAT";
 	case L_DELIMITER:
-		return "delimiter";
+		return "DELIMITER";
 	case L_BRACE_CLOSING:
-		return "closing brace";
+		return "CLOSING BRACE";
 	case L_BRACE_OPENING:
-		return "opening brace";
+		return "OPENING BRACE";
 	case  L_CONSTANT:
-		return "constant";
+		return "CONSTANT";
 	case L_CONSTANT_BIN:
-		return "constant binary";
+		return "CONSTANT BINARY";
 	case L_CONSTANT_HEX:
-		return "constant hexademical";
+		return "CONSTANT HEXADEMICAL";
 	case L_IDENTIFIER:
-		return "identifier";
+		return "IDENTIFIER";
 	case L_KEYWORD:
-		return "PASkeyword";
+		return "KEYWORD";
 	case L_OPERAT_ARITHMETIC:
-		return "arithmetic op";
+		return "ARITHMETIC OP";
 	case L_OPERAT_ASSIGNMENT:
-		return "assignment op";
+		return "ASSIGNMENT OP";
 	case L_OPERAT_BITWISE:
-		return "bitwise op";
+		return "BITWISE OP";
 	case L_OPERAT_LOGIC:
-		return "logic op";
+		return "LOGIC OP";
 	case L_OPERAT_RELATION:
-		return "relation op";
+		return "RELATION OP";
 	case L_UNACCEPTABLE_CHAR:
 		return "ERR UNACCEPTABLE CHAR";
 	case L_UNACCEPTABLE_WORD:
