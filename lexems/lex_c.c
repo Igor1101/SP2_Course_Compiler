afec0c801ba5d9d2a307f687b19af37a717ca730
Igor1101 <Igor1101@github.com>
2019-10-29 09:00:14 +0200

    c: Added c dependent sources

---
+++ lexems/lex_c.c
@@ -0,0 +1,593 @@
+/*
+ * lex.c
+ *
+ *  Created on: Sep 29, 2019
+ *      Author: igor
+ */
+#include <defs.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <lexems/utf8.h>
+#include <lexems/tables.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "lex.h"
+
+static const char * c_reservedwords[] = {
+		"auto", "signed", "const", "extern", "register", "unsigned", "static",
+		"int", "float", "char", "double", "long",
+		"void",
+		"if", "else", "switch", "case", "default",
+		"break", "continue",
+		"for", "while", "do",
+		"goto",
+		"return",
+		"enum",
+		"sizeof",
+		"struct", "typedef", "union",
+		"volatile"
+};
+
+static const char* c_operators[] = {
+		"+", "-", "*", "/", "%",
+		"++", "--",
+		"=", "+=", "-=", "*=", "/=", "%=",
+		"==", ">","<", "!=", ">=", "<=",
+		"&&", "||", "!",
+		"&", "|", "^", "~", "<<", ">>", "->", "."
+};
+static const char* c_op_arithmetic[] = {
+		"+", "-", "*", "/", "%", "++", "--"
+};
+static const char* c_op_assign[] = {
+		"=", "+=", "-=", "*=", "/=", "%="
+};
+static const char* c_op_bitwise[] = {
+		"&", "|", "^", "~", "<<", ">>"
+};
+static const char* c_op_logic[] = {
+		"&&", "||", "!",
+};
+static const char* c_op_relation[] = {
+		"==", ">","<", "!=", ">=", "<=",
+};
+
+bool is_char_in(unsigned ch, const char*str)
+{
+	if(str == NULL) {
+		return false;
+	}
+	if(ch > 255) {
+		return false;/* dont support utf-8 */
+	}
+	for(int i=0; i<strlen(str); i++) {
+		if(ch == str[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool is_name(char* str)
+{
+
+	if(str == NULL)
+		return false;
+	if(!is_sacc_char(str[0]))
+		return false;
+	for(int i=0; i<strlen(str); i++) {
+		if(!is_acc_char(str[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+char* get_next_lexem_alloc(char*str, int* i, lexem_t* lexerror)
+{
+	char*lex;
+	const char* op_ch = "?!~|%^&*-+=/<>";
+	if(str == NULL)
+		return NULL;
+	unsigned ch = u8_nextchar(str, i);
+	pr_debug("char %c indentifying", ch);
+	if(is_char_in(ch, ";,:. ")) {
+		pr_debug("lex is delimiter");
+		lex = calloc(1, 2);
+		lex[0] = (char)ch;
+		lex[1] = 0;
+		return lex;
+	}
+	if(is_char_in(ch, "()[]{}")) {
+		pr_debug("lex is brace");
+		lex = calloc(1, 2);
+		lex[0] = (char)ch;
+		lex[1] = 0;
+		return lex;
+	}
+	char* get_full_lexem(unsigned ch)
+	{
+		char*lex = calloc(1, 1);
+		int index = 0;
+		do {
+			if(ch > 255) {
+				ch = '?';
+				*lexerror = L_UNACCEPTABLE_CHAR;
+			}
+			lex = reallocarray(lex, index+1, 1);
+			lex[index++] = ch;
+			ch = u8_nextchar(str, i);
+		} while((ch != 0 ) &&
+				(!isspace(ch)) &&
+				(!is_char_in(ch, ";,:")) &&
+				(!is_char_in(ch, op_ch)) &&
+				(!is_char_in(ch, "()[]{}<>")));
+		/* set null char */
+		lex = reallocarray(lex, index+1, 1);
+		lex[index] = '\0';
+		/* move to previous char */
+		u8_dec(str, i);
+		pr_debug("alloc %s", lex);
+		return lex;
+	}
+	if(is_char_in(ch, op_ch)) {
+		pr_debug("lexem may be C operator");
+		lex = calloc(1, 1);
+		int index = 0;
+		do {
+			if(ch > 255) {
+				ch = '?';
+				*lexerror = L_UNACCEPTABLE_CHAR;
+			}
+			lex = reallocarray(lex, index+1, 1);
+			lex[index++] = ch;
+			ch = u8_nextchar(str, i);
+		} while(is_char_in(ch, op_ch));
+		pr_debug("alloc %s", lex);
+		/* set null char */
+		lex = reallocarray(lex, index+1, 1);
+		lex[index] = '\0';
+		/* move to previous char */
+		u8_dec(str, i);
+		return lex;
+	}
+	if(is_sacc_char(ch)) {
+		/* this may be identifier */
+		return get_full_lexem(ch);
+	}
+	if(is_char_in(ch, "0123456789") ) {
+		/* does look like dec binary or hex number */
+		return get_full_lexem(ch);
+	}
+	/* something is unknown */
+	lex = calloc(1,2);
+	lex[0] = ch;
+	lex[1] = 0;
+	return lex;
+}
+
+
+int lex_parse(char*str)
+{
+	str_array_remove();
+	lexem_t lastlexem = L_NOTDEFINED;
+	int ret_status = 0;
+	int i = 0;
+	while(1) {
+		unsigned ch = u8_nextchar(str, &i);
+		if(ch == 0) {
+			break;
+		}
+		if(ch > 255 ) {
+			pr_err("non ascii character detected outside quatation");
+			ret_status++;
+			continue;
+		}
+		if(is_char_in(ch, ";,:")) {
+			pr_debug("found delimiter");
+			char _del[2];
+			_del[0] = ch;
+			_del[1] = 0;
+			str_add(_del, L_DELIMITER);
+			lastlexem = L_DELIMITER;
+			continue;
+		}
+		if(isspace(ch)) {
+			pr_debug("ignoring space");
+			continue;
+		}
+		if(is_char_in(ch, "([{")) {
+			pr_debug("found opening brace");
+			char _br[2];
+			_br[0] = ch;
+			_br[1] = 0;
+			str_add(_br, L_BRACE_OPENING);
+			lastlexem = L_BRACE_OPENING;
+			continue;
+		}
+		if(is_char_in(ch, ")]}")) {
+			pr_debug("found closing brace");
+			char _br[2];
+			_br[0] = ch;
+			_br[1] = 0;
+			str_add(_br, L_BRACE_CLOSING);
+			lastlexem = L_BRACE_CLOSING;
+			continue;
+		}
+		if(is_char_in(ch, "'")) {
+			/* looks like char symbol */
+			pr_debug("looks like char");
+			unsigned ch1 = u8_nextchar(str, &i);
+			char ch2 = u8_nextchar(str, &i);
+			if(is_char_in(ch2, "'") && ch1 < 255) {
+				/* it is char symbol */
+				char s[4] = { ch, (char)ch1, ch2, 0};
+				str_add(s, L_CHAR);
+				lastlexem = L_CHAR;
+				continue;
+			} else {
+				u8_dec(str, &i);
+				u8_dec(str, &i);
+			}
+		}
+		if(is_char_in(ch, "\"")) {
+			/* looks like string */
+			pr_debug("string process");
+			/* save str pointer */
+			int iold = i;
+			char* strend = strchr(&str[i+1], '"');
+			if(strend == NULL) {
+				pr_err("string without end \" ");
+			} else {
+				int iend = (int)(strend - &str[i]+2);
+				char*newstr = str_alloc(&str[i - 1]);
+				char*newstrend = strchr(newstr+1, '"');
+				if(newstrend == NULL) {
+					free(newstr);
+				} else {
+					*(newstrend+1) = '\0';
+					str_add(newstr, L_STRING);
+					lastlexem = L_STRING;
+					for(int j=0; j<u8_strlen(newstr)-1;j++) {
+						u8_inc(str, &i);
+					}
+					continue;
+				}
+			}
+		}
+		/* parse lexem to find out what is it */
+		lexem_t lerror ;
+		/* may be we are just at the beginning
+		 * of lexem */
+		u8_dec(str, &i);
+		lerror = L_NOTDEFINED;
+		char* lex = get_next_lexem_alloc(str, &i, &lerror);
+		if(lex == NULL) {
+			return ret_status;
+		}
+		if(lerror == L_UNACCEPTABLE_CHAR || lerror == L_UNACCEPTABLE_WORD) {
+			str_add(lex, lerror);
+			ret_status++;
+			continue;
+		}
+		if(is_str_in(lex, c_reservedwords, sizeof c_reservedwords)) {
+			pr_debug("found keyword");
+			str_add(lex, L_KEYWORD);
+			lastlexem = L_KEYWORD;
+		} else if(is_name(lex)) {
+			pr_debug("found identifier");
+			str_add(lex, L_IDENTIFIER);
+			lastlexem = L_IDENTIFIER;
+		} else if(is_hex(lex)) {
+			pr_debug("found hex number");
+			str_add(lex, L_CONSTANT_HEX);
+			lastlexem = L_CONSTANT_HEX;
+		} else if(is_dec(lex)) {
+			pr_debug("found decimal number");
+			str_add(lex, L_CONSTANT);
+			lastlexem = L_CONSTANT;
+		} else if(is_op_chars(lex)) {
+			pr_debug("may be operator");
+			while(strlen(lex) > 0) {
+				pr_debug("lex=%s", lex);
+				if(is_str_in(lex, c_operators, sizeof c_operators)) {
+					if(is_str_in(lex, c_op_arithmetic, sizeof c_op_arithmetic)) {
+						/* verify if it is arithmetic or
+						 * pointer
+						 */
+						if(!strcmp(lex, "*")) {
+							if(lastlexem == L_BRACE_OPENING ||
+									lastlexem == L_DELIMITER ||
+									lastlexem == L_NOTDEFINED ||
+									lastlexem == L_KEYWORD ||
+									lastlexem == L_OPERAT_ARITHMETIC||
+									lastlexem == L_OPERAT_ASSIGNMENT||
+									lastlexem == L_OPERAT_BITWISE||
+									lastlexem == L_OPERAT_LOGIC||
+									lastlexem == L_OPERAT_RELATION||
+									lastlexem == L_POINTER) {
+								/* this looks like pointer! */
+								pr_debug("pointer found!");
+								str_add(lex, L_POINTER);
+								lastlexem = L_POINTER;
+								break;
+							}
+						}
+						str_add(lex, L_OPERAT_ARITHMETIC);
+						lastlexem = L_OPERAT_ARITHMETIC;
+						break;
+					}
+					if(is_str_in(lex, c_op_assign, sizeof c_op_assign)) {
+						str_add(lex, L_OPERAT_ASSIGNMENT);
+						lastlexem = L_OPERAT_ASSIGNMENT;
+						break;
+					}
+					if(is_str_in(lex, c_op_bitwise, sizeof c_op_bitwise)) {
+					/* verify if it is arithmetic or
+					* pointer
+					*/
+						if(!strcmp(lex, "&")) {
+							if(lastlexem == L_BRACE_OPENING ||
+									lastlexem == L_DELIMITER ||
+									lastlexem == L_NOTDEFINED ||
+									lastlexem == L_KEYWORD ||
+									lastlexem == L_OPERAT_ARITHMETIC||
+									lastlexem == L_OPERAT_ASSIGNMENT||
+									lastlexem == L_OPERAT_BITWISE||
+									lastlexem == L_OPERAT_LOGIC||
+									lastlexem == L_OPERAT_RELATION) {
+								/* this looks like pointer! */
+								pr_debug("pointer found!");
+								str_add(lex, L_POINTER);
+								lastlexem = L_POINTER;
+								break;
+							}
+						}
+						str_add(lex, L_OPERAT_BITWISE);
+						lastlexem = L_OPERAT_BITWISE;
+						break;
+					}
+					if(is_str_in(lex, c_op_logic, sizeof c_op_logic)) {
+						str_add(lex, L_OPERAT_LOGIC);
+						lastlexem = L_OPERAT_LOGIC;
+						break;
+					}
+					if(is_str_in(lex, c_op_relation, sizeof c_op_relation)) {
+						str_add(lex, L_OPERAT_RELATION);
+						lastlexem = L_OPERAT_RELATION;
+						break;
+					}
+					const char*struct_p[] = {"->" };
+					if(is_str_in(lex, struct_p, sizeof struct_p)){
+						/* struct pointer  */
+						str_add(lex, L_STRUCT_POINTER);
+						lastlexem = L_STRUCT_POINTER;
+						break;
+					}
+					const char*struct_elem[] = {"." };
+					if(is_str_in(lex, struct_elem, sizeof struct_elem)){
+						/* if point is standalone
+						 * */
+						str_add(lex, L_UNKNOWN_WORD);
+						lastlexem = L_UNKNOWN_WORD;
+						ret_status++;
+						break;
+					}
+				}
+				if(strlen(lex) == 1) {
+					str_add(lex, L_UNKNOWN_WORD);
+					lastlexem = L_UNKNOWN_WORD;
+					ret_status++;
+					break;
+				}
+				lex[strlen(lex)-1] = '\0';
+				i--;
+			}
+		} else if(is_float(lex)){
+			pr_debug("found floating number");
+			str_add(lex, L_CONSTANT_FLOAT);
+			lastlexem = L_CONSTANT_FLOAT;
+		} else if(is_structident(lex)) {
+			pr_debug("found struct element");
+			/* separate parts of struct element */
+			char*next_word = lex;
+			char*point = ".";
+			char*next_point = strchr(next_word, '.');
+			while(next_point != NULL) {
+				*next_point = '\0';
+				str_add(next_word, L_IDENTIFIER);
+				str_add(point, L_STRUCT_DELIMITER);
+				next_word = next_point+1;
+				next_point = strchr(next_word, '.');
+			}
+			str_add(next_word, L_IDENTIFIER);
+		}
+		else {
+			pr_warn("unknown word detected");
+			str_add(lex, L_UNKNOWN_WORD);
+			ret_status++;
+		}
+		free(lex);
+	}
+	return ret_status;
+}
+
+bool is_hex(char* lex)
+{
+	if(lex == NULL)
+		return false;
+	if(strlen(lex)<3)
+		return false;
+	if(lex[0] != '0' || (lex[1] != 'x' && lex[1] != 'X'))
+		return false;
+	for(int i=2; i<strlen(lex); i++) {
+		if(!is_char_in(lex[i], "1234567890abcdefABCDEF")) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool is_dec(char*lex)
+{
+	if(lex == NULL)
+		return false;
+	for(int i=0; i<strlen(lex); i++) {
+		if(!isdigit(lex[i]))
+			return false;
+	}
+	return true;
+}
+bool is_float(char*lex)
+{
+	bool point_once = false;
+	bool exp_once = false;
+	if(lex == NULL)
+		return false;
+	for(int i=0; i<strlen(lex); i++) {
+		if(!isdigit(lex[i])) {
+			if(lex[i] == '.' && i<(strlen(lex) - 1)) {
+				if(point_once)
+					return false;
+				else {
+					point_once = true;
+				}
+			} else if((lex[i] == 'e' || lex[i] == 'E') && i<strlen(lex) - 1){
+				if(exp_once)
+					return false;
+				else {
+					exp_once = true;
+				}
+			} else if((lex[i] == '-' || lex[i] == '+') && i<strlen(lex) - 1 &&
+					i > 1) {
+				if(lex[i-1] == 'E' || lex[i-1] == 'e') {
+					continue;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+const char*acc_start =
+			"QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm_";
+const char*acc =
+			"QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm_1234567890";
+bool is_acc_char(char c)
+{
+	return is_char_in(c, acc);
+}
+
+bool is_sacc_char(char c)
+{
+	return is_char_in(c, acc_start);
+}
+
+bool is_op_chars(char*str)
+{
+	for(int i=0; i<strlen(str); i++) {
+		if(!is_char_in(str[i], ".~|&!-+/<>=^%*")) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool is_str_in(char*str, const char*arr[], size_t arr_size)
+{
+	if(str == NULL) {
+		return false;
+	}
+	for(int i=0; i<arr_size / sizeof arr[0]; i++) {
+		if(!strcmp(str, arr[i]))
+			return true;
+	}
+	return false;
+}
+
+bool is_structident(char*str)
+{
+	bool lastp_near = false;
+	bool is_point(char ch)
+	{
+		if(ch=='.') {
+			if(lastp_near) {
+				return false;
+			} else {
+				lastp_near = true;
+				return true;
+			}
+		} else {
+			lastp_near = false;
+		}
+		return false;
+	}
+	if(!is_sacc_char(str[0])) {
+		return false;
+	}
+	if((str[strlen(str)-1] == '.')) {
+		return false;
+	}
+	for(int i=1; i<strlen(str); i++) {
+		if((!is_acc_char(str[i])) && (!is_point(str[i])))
+			return false;
+		is_point(str[i]);
+	}
+	return true;
+}
+char* lex_to_str(lexem_t lt)
+{
+	switch(lt) {
+	case L_STRING:
+		return "string";
+	case L_CHAR:
+		return "char";
+	case L_STRUCT_DELIMITER:
+		return "struct delimiter";
+	case L_STRUCT_POINTER:
+		return "struct pointer";
+	case L_POINTER:
+		return "pointer";
+	case L_CONSTANT_FLOAT:
+		return "constant float";
+	case L_DELIMITER:
+		return "delimiter";
+	case L_BRACE_CLOSING:
+		return "closing brace";
+	case L_BRACE_OPENING:
+		return "opening brace";
+	case  L_CONSTANT:
+		return "constant";
+	case L_CONSTANT_BIN:
+		return "constant binary";
+	case L_CONSTANT_HEX:
+		return "constant hexademical";
+	case L_IDENTIFIER:
+		return "identifier";
+	case L_KEYWORD:
+		return "Ckeyword";
+	case L_OPERAT_ARITHMETIC:
+		return "arithmetic op";
+	case L_OPERAT_ASSIGNMENT:
+		return "assignment op";
+	case L_OPERAT_BITWISE:
+		return "bitwise op";
+	case L_OPERAT_LOGIC:
+		return "logic op";
+	case L_OPERAT_RELATION:
+		return "relation op";
+	case L_UNACCEPTABLE_CHAR:
+		return "ERR UNACCEPTABLE CHAR";
+	case L_UNACCEPTABLE_WORD:
+		return "ERR UNACCEPTABLE WORD";
+	case L_UNKNOWN_WORD:
+		return "ERR UNKNOWN WORD";
+	default:
+		return "UNKNOWN LEXEM";
+	}
+}
---
+++ syntax/syntax_c.c
@@ -0,0 +1,519 @@
+/*
+ * analyzer.c
+ *
+ *  Created on: Oct 9, 2019
+ *      Author: igor
+ */
+#include <lexems/lex.h>
+#include <lexems/tables.h>
+#include <defs.h>
+#include "syntax.h"
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+syn_state_t st;
+int err_amount = 0;
+void init_syn_analyzer(void)
+{
+	st.num = 0;
+	st.arithmetic = false;
+	st.assignment = false;
+	st.curlybrace_nest = 0;
+	st.nesting = 0;
+	st.parensis_nest = 0;
+	st.square_braces_nest = 0;
+	st.symbols_after_del = 0;
+	err_amount = 0;
+}
+int syn_analyze(void)
+{
+	init_syn_analyzer();
+	for(st.num=0; st.num<str_array.amount; ) {
+		if(str_get(st.num) == NULL) {
+			pr_err("SYNTAX: NULL str_t detected");
+			err_amount++;
+			st.num++;
+			continue;
+		}
+		if(str_get(st.num)->inst == NULL) {
+			pr_err("SYNTAX: NULL string detected");
+			err_amount++;
+			st.num++;
+			continue;
+		}
+		switch(str_get(st.num)->lext) {
+		case L_IDENTIFIER:
+			st.num = process_ident(st.num, st.nesting+1, false, false);
+			break;
+		case L_OPERAT_ARITHMETIC:
+			st.num = process_expression(st.num, st.nesting+1, false, false);
+			break;
+		case L_BRACE_CLOSING:
+			//close_brace(st.num);
+		case L_BRACE_OPENING:
+			//open_brace(st.num);
+		case L_DELIMITER:
+			st.num = is_delimiter_next_expected(st.num, st.nesting);
+			break;
+		default:
+			pr_err("UNEXPECTED SYMBOL");
+
+			err_amount++;
+			st.num++;
+		}
+		if(st.num >= str_array.amount) {
+			if(str_array.amount>0 && (str_get(str_array.amount - 1)->lext != L_DELIMITER)) {
+				set_synt_err(str_array.amount - 1, S_DEL);
+				err_amount++;
+			}
+			return err_amount;
+		}
+		st.num = is_delimiter_next_expected(st.num, st.nesting);
+    }
+	if(str_array.amount>0 && (str_get(str_array.amount - 1)->lext != L_DELIMITER)) {
+		set_synt_err(str_array.amount - 1, S_DEL);
+		err_amount++;
+	}
+	return err_amount;
+}
+
+int is_delimiter_next_expected(int num, int level)
+{
+	if(num>=str_array.amount) {
+		err_amount++;
+		pr_err("DELIMITER EXPECTED AT THE END"
+	"EOF FOUND");
+	}
+	else if(str_get(num)->lext == L_DELIMITER) {
+		str_get(num)->level = level;
+		str_get(num)->synt = S_DEL;
+	} else {
+		set_synt_err(num, S_DEL);
+		err_amount++;
+	}
+	return num+1;
+}
+
+int close_brace(int num)
+{
+	int ret;
+	switch(*(str_get(num)->inst)) {
+	case ')':
+		ret = --st.parensis_nest;
+		break;
+	case '}':
+		ret = --st.curlybrace_nest;
+		break;
+	case ']':
+		ret = --st.parensis_nest;
+		break;
+	default:
+		pr_warn("close_brace garbage detected");
+	}
+	return ret;
+}
+
+int open_brace(int num)
+{
+	int ret;
+	switch(*(str_get(num)->inst)) {
+	case '(':
+		ret = ++st.parensis_nest;
+		break;
+	case '{':
+		ret = ++st.curlybrace_nest;
+		break;
+	case '[':
+		ret = ++st.parensis_nest;
+		break;
+	default:
+		pr_warn("open_brace garbage detected %c", *(str_get(num)->inst));
+	}
+	return ret;
+}
+
+int push(syn_t op)
+{
+	if(st.__op_p >= MAX_DEPTH) {
+		pr_err("op push overflow");
+		return -1;
+	}
+	st.op[st.__op_p++] = op;
+	return 0;
+}
+syn_t pop(void)
+{
+	if(st.__op_p == 0) {
+		pr_err("op pop is underflow");
+		return S_NOTDEFINED;
+	}
+	return st.op[st.__op_p--];
+}
+
+void reset(void)
+{
+	st.__op_p = 0;
+}
+
+int process_ident(int num, int level, bool maybeparam, bool inside_expr)
+{
+	if(num < (str_array.amount-1)) {
+
+		if(str_get(num)->lext == L_IDENTIFIER &&
+			!strcmp(str_get(num + 1)->inst, "[")) {
+			int i = process_array(num, level);
+			return i;
+		}
+		if(str_get(num+1)->lext == L_BRACE_OPENING &&
+				!strcmp(str_get(num+1)->inst,"(")) {
+			/* this is a function */
+			pr_debug("function %s", str_get(num)->inst);
+			int i = process_function(num, level);
+			return i;
+		}
+		if(
+				str_get(num+1)->lext == L_OPERAT_ARITHMETIC||
+				str_get(num+1)->lext == L_OPERAT_ASSIGNMENT||
+				str_get(num+1)->lext == L_OPERAT_BITWISE||
+				str_get(num+1)->lext == L_OPERAT_LOGIC||
+				str_get(num+1)->lext == L_OPERAT_RELATION||
+				str_get(num-1)->lext == L_OPERAT_ARITHMETIC||
+				str_get(num+1)->lext == L_OPERAT_ASSIGNMENT||
+				str_get(num-1)->lext == L_OPERAT_BITWISE||
+				str_get(num-1)->lext == L_OPERAT_LOGIC||
+				str_get(num-1)->lext == L_OPERAT_RELATION||
+				str_get(num+1)->lext == L_OPERAT_ASSIGNMENT
+				) {
+			if(inside_expr) {
+				str_get(num)->synt = S_ID_VARIABLE;
+				str_get(num)->level = level;
+				return ++num;
+			} else {
+				return process_expression(num, level, maybeparam, false);
+			}
+		}
+		if(maybeparam && (str_get(num+1)->lext == L_BRACE_CLOSING ||
+				str_get(num+1)->lext == L_DELIMITER)) {
+			str_get(num)->synt = S_ID_VARIABLE;
+			str_get(num)->level = level;
+			return ++num;
+		}
+	}
+	if(st.symbols_after_del > 0 && num > 0) {
+		if(
+				str_get(num-1)->lext == L_OPERAT_ARITHMETIC||
+				str_get(num-1)->lext == L_OPERAT_ASSIGNMENT||
+				str_get(num-1)->lext == L_OPERAT_BITWISE||
+				str_get(num-1)->lext == L_OPERAT_LOGIC||
+				str_get(num-1)->lext == L_OPERAT_RELATION) {
+			str_get(num)->synt = S_ID_VARIABLE;
+			str_get(num)->level = level;
+			return ++num;
+		}
+	}
+	err_amount++;
+	str_get(num)->synt = S_ID_UNDEFINED;
+	str_get(num)->level = level;
+	return ++num;
+}
+
+int process_function(int num, int level)
+{
+	bool has_del = false;
+	int level_del = level+3;
+	int level_param = level+3;
+	if(str_get(num)->lext == L_IDENTIFIER) {
+		str_get(num)->synt = S_ID_FUNCTION;
+		str_get(num)->level = level+1;
+	}
+	if(str_get(num+1)->lext == L_BRACE_OPENING) {
+		str_get(num+1)->synt = S_FUNC_BRACE_OPEN;
+		str_get(num+1)->level = level+2;
+		pr_debug("opening func brace");
+	}
+	int next_del = next_delimiter(num, level, false);
+	if(next_del<0) {
+		pr_warn("could not found next delimiter");
+		next_del = str_array.amount;
+	}
+	for(int i=num+2; i<next_del; ) {
+		switch(str_get(i)->lext) {
+		case L_BRACE_CLOSING:
+			if(!strcmp(str_get(i)->inst, ")") ) {
+				pr_debug("closing func brace");
+				set_synt(i, S_FUNC_BRACE_CLOSE);
+				str_get(i)->level = level+2;
+				return i+1;
+			}
+			/* unexpected brace */
+			set_synt_err(num, S_FUNC_BRACE_CLOSE);
+
+			err_amount++;
+			return i+1;
+		case L_IDENTIFIER:
+		{
+			int next = process_ident(i, level_param, true, false);
+			if(str_get(i)->synt == S_ID_PARAM) {
+				pr_debug("it was param");
+			}
+			i = next;
+			break;
+		}
+		case L_CHAR:
+		case L_CONSTANT:
+		case L_CONSTANT_BIN:
+		case L_CONSTANT_FLOAT:
+		case L_CONSTANT_HEX:
+		case L_STRING:
+			str_get(i)->synt = S_CONST_PARAM;
+			str_get(i)->level = level_param;
+			i++;
+			break;
+		case L_DELIMITER:
+			if(!strcmp(str_get(i)->inst, ",")) {
+				if(has_del == false) {
+					/* modify settings */
+					has_del = true;
+					level_param = level+4;
+					i = num+2;
+					continue;
+				}
+				str_get(i)->synt = S_DEL_PARAM;
+				str_get(i)->level = level_del;
+				i++;
+				break;
+			}
+			set_synt(i, S_DEL_PARAM);
+			i++;
+			break;
+		default:
+			str_get(i)->synt = S_ID_UNDEFINED;
+			str_get(i)->level = level_param;
+			i++;
+		}
+	}
+	pr_err("Expected ')' brace at end of input");
+	err_amount++;
+	return str_array.amount;
+}
+
+int process_expression(int num, int level, bool inside_expr, bool inside_array)
+{
+	syn_t prev = S_NOTDEFINED;
+	syn_t expect = S_NOTDEFINED;
+	bool unary_been = false;
+	int numlevel = level + 2;
+	int arithlevel = level + 1;
+	int next_del = next_delimiter(num, level, true);
+	if(next_del<0) {
+		pr_warn("could not find delimiter when processing expression");
+		next_del = str_array.amount;
+	}
+	if(inside_expr) {
+		int brace = next_closing_brace(num, level);
+		if(brace>=0 && brace < next_del) {
+			next_del = brace;
+		}
+	}
+	for(; num<next_del;) {
+		switch(str_get(num)->lext) {
+		case L_CONSTANT:
+		case L_CONSTANT_BIN:
+		case L_CONSTANT_FLOAT:
+		case L_CONSTANT_HEX:
+		case L_CHAR:
+			if(prev == S_CONST_PARAM) {
+				err_amount++;
+				set_synt_err_unexp(num, S_OPERAT_BINARY, S_CONST_PARAM);
+				num++;
+				break;
+			}
+			prev = S_CONST_PARAM;
+			str_get(num)->synt = S_CONST;
+			str_get(num)->level = numlevel;
+			num++;
+			break;
+		case L_OPERAT_ASSIGNMENT:
+			if(prev == S_ID_VARIABLE &&
+					(str_get(num+1)->lext == L_CHAR ||
+					str_get(num+1)->lext == L_CONSTANT||
+					str_get(num+1)->lext == L_CONSTANT_BIN||
+					str_get(num+1)->lext == L_CONSTANT_FLOAT||
+					str_get(num+1)->lext == L_CONSTANT_HEX||
+					str_get(num+1)->lext == L_IDENTIFIER||
+					str_get(num+1)->lext == L_OPERAT_ARITHMETIC)
+					) {
+				str_get(num)->synt = S_OPERAT_ASSIGNMENT;
+				str_get(num)->level = arithlevel;
+				num++;
+				return process_expression(num, level+1, inside_expr, inside_array);
+			} else {
+				err_amount++;
+				set_synt_err_unexp(num, S_OPERAT_BINARY, S_OPERAT_ASSIGNMENT);
+				num++;
+				break;
+			}
+		case L_OPERAT_ARITHMETIC:
+		case L_OPERAT_BITWISE:
+		case L_OPERAT_RELATION:
+		case L_OPERAT_LOGIC:
+		{
+			const char* unary[] = { "++", "--"};
+			const char* sign[] = { "-", "+", "!"};
+			const char* binary[] = {
+					"-", "+", "*", "/",
+					"==", ">","<", "!=", ">=", "<=",
+					"&&", "||",
+					"&", "|", "^", "~", "<<", ">>"
+			};
+			if(is_str_in(str_get(num)->inst, unary, sizeof unary)) {
+				pr_debug("unary op detected num=%d", num);
+				if(prev == S_ID_VARIABLE || str_get(num+1)->lext == L_IDENTIFIER) {
+					prev = S_OPERAT_UNARY;
+					expect = S_NOTDEFINED;
+					unary_been = true;
+					str_get(num)->synt = S_OPERAT_UNARY;
+					str_get(num)->level = numlevel + 1;
+					num++;
+				} else {
+					prev = S_OPERAT_UNARY;
+					set_synt_err_unexp(num, S_ID_UNDEFINED, S_OPERAT_UNARY);
+					str_get(num)->level = numlevel + 1;
+					err_amount++;
+					num++;
+				}
+				break;
+			}
+			if(is_str_in(str_get(num)->inst, sign, sizeof sign)) {
+				pr_debug("sign op detected num=%d", num);
+				if((str_get(num+1)->lext == L_IDENTIFIER||
+						str_get(num+1)->lext == L_CONSTANT ||
+						str_get(num+1)->lext == L_CONSTANT_FLOAT ||
+						str_get(num+1)->lext == L_CONSTANT_BIN ||
+						str_get(num+1)->lext == L_CONSTANT_HEX
+						)&&
+						str_get(num-1)->lext != L_IDENTIFIER) {
+					prev = S_OPERAT_UNARY;
+					expect = S_ID_VARIABLE;
+					unary_been = true;
+					str_get(num)->synt = S_OPERAT_UNARY;
+					str_get(num)->level = numlevel + 1;
+					num++;
+					break;
+				}
+			}
+			if(is_str_in(str_get(num)->inst, binary, sizeof binary)) {
+				pr_debug("binary op num=%d", num);
+				if((str_get(num+1)->lext == L_IDENTIFIER||
+						str_get(num+1)->lext == L_CONSTANT ||
+						str_get(num+1)->lext == L_CONSTANT_FLOAT ||
+						str_get(num+1)->lext == L_CONSTANT_BIN ||
+						str_get(num+1)->lext == L_CONSTANT_HEX
+						)&& (
+						str_get(num-1)->lext == L_IDENTIFIER||
+						str_get(num-1)->lext == L_CONSTANT ||
+						str_get(num-1)->lext == L_CONSTANT_FLOAT ||
+						str_get(num-1)->lext == L_CONSTANT_BIN ||
+						str_get(num-1)->lext == L_CONSTANT_HEX ||
+						prev == S_ID_VARIABLE
+						)) {
+					expect = S_ID_VARIABLE;
+					prev = S_OPERAT_BINARY;
+					unary_been = true;
+					str_get(num)->synt = S_OPERAT_BINARY;
+					str_get(num)->level = arithlevel;
+					num++;
+					break;
+				} else {
+					err_amount++;
+					set_synt_err_unexp(num, S_CONST, S_OPERAT_BINARY);
+					str_get(num)->level = arithlevel;
+					num++;
+					break;
+				}
+			}
+
+			break;
+		}
+		case L_DELIMITER:
+			return num;
+		case L_IDENTIFIER:
+			if(prev == S_ID_VARIABLE) {
+				err_amount++;
+				set_synt_err_unexp(num, S_OPERAT_BINARY, S_ID_VARIABLE);
+				num++;
+				break;
+			}
+			num = process_ident(num, numlevel, true, true);
+			prev = S_ID_VARIABLE;
+			break;
+		case L_BRACE_CLOSING:
+			if(!strcmp(str_get(num)->inst, "]")&& inside_array) {
+				return num;
+			}
+		default:
+			err_amount++;
+			pr_err("Unexpected operator");
+			str_get(num)->level = 0;
+			set_synt_err_unexp(num, S_NOTDEFINED, lex_to_syn(str_get(num)->lext));
+			num++;
+		}
+	}
+	return num;
+}
+
+int next_delimiter(int num, int level, bool param)
+{
+	for(int i=num; i<str_array.amount; i++) {
+		if(str_get(i)->lext == L_DELIMITER ) {
+			if(param && !strcmp(str_get(i)->inst, ",")) {
+				return i;
+			}
+			else if(!strcmp(str_get(i)->inst, ";")) {
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+int next_closing_brace(int num, int level)
+{
+	for(int i=num; i<str_array.amount; i++) {
+		if(!strcmp(str_get(i)->inst, ")") ) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int process_array(int num, int level)
+{
+	if(
+			str_get(num)->lext == L_IDENTIFIER &&
+			!strcmp(str_get(num + 1)->inst, "[")
+			) {
+		str_get(num)->synt = S_ID_VARIABLE;
+		str_get(num)->level = level;
+		str_get(num+1)->synt = S_BRACE_OPEN;
+		str_get(num+1)->level = level+1;
+		num = process_expression(num+2, level+2, true, true);
+		if(num>=str_array.amount)
+			return num++;
+		if( !strcmp(str_get(num)->inst, "]")) {
+			str_get(num)->synt = S_BRACE_CLOSE;
+			str_get(num)->level = level+1;
+			return num+1;
+		} else {
+			set_synt_err(num, S_BRACE_CLOSE);
+			str_get(num)->level = level+1;
+			return num+1;
+		}
+	} else {
+		return ++num;
+	}
+	return num++;
+}
+
